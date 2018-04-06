1a70e88e367f42364298f0a97f3819c41f8fba21
Brandon Sims <brandon.w.sims@gmail.com>
2018-04-05 22:40:49 -0500

    Better working parser now except for some nested objects

--- parse.py
+++ parse.py
@@ -8,8 +8,14 @@ if __name__ == '__main__':
     # Treats source code to be parsed as bytes like file in memory
     src = BytesIO(open('template_advanced.py', 'r').read().encode('utf-8'))
 
+    # String for storing the html to be generated for parsing the source
+    html = ''
+
     # For each line in the source code
     for line in src:
+        # Start off by opening a li tag for the html generation
+        li = '<li>\n\t'
+
         # Store line as plain text
         plain = line.decode('utf-8').replace('\n', '')
 
@@ -23,10 +29,38 @@ if __name__ == '__main__':
         tokens = [token.tok_name[t.type] for t in tokens]
         tokens = tokens[1:-1]
 
+        first = True
+        for lex, tok in zip(lexemes, tokens):
+            if first:
+                first = False
+                indent = lex.count('\t')
+                li += '<span class="indent-level-{}"></span>'.format(indent)
+
+            if keyword.iskeyword(lex):
+                style = 'class="function-call"'
+            elif tok == 'NUMBER':
+                style = 'class="literal"'
+            elif tok == 'STRING':
+                style = 'class="string"'
+            elif tok == 'OP':
+                style = 'class="keyword"'
+            else:
+                style = 'class=""'
+
+            if tok not in ['NEWLINE', 'NL', 'DEDENT', 'INDENT']:
+                li += '<span {}>{} </span>'.format(style, lex)
+
+        # Close li tag
+        li += '\n</li>'
+
+        # Append to html
+        html += li
+
         # For debugging
-        print(plain)
-        print(lexemes)
-        print(tokens)
+        # print(plain)
+        # print(lexemes)
+        # print(tokens)
+        print(li)
 
     # Skip encoding line
     # next(src)
--- template_advanced.py
+++ template_advanced.py
@@ -1,4 +1,3 @@
-    test = 5
 from io import BytesIO
 import tokenize
 import token
@@ -9,45 +8,33 @@ if __name__ == '__main__':
 	src = BytesIO(open('parse.py', 'r').read().encode('utf-8'))
 	src = tokenize.tokenize(src.readline)
 
-	# For storing parsed source information
 	tokens = [[]]
 	text = [[1, 2, 3], ['4', '5', '6'], [1, 2, 3], ['4', '5', '6']]
 
-	# Skip the encoding token associated with file
 	next(src)
 
-	# For each line in the source
 	for t in src:
-		# Get name of current token
 		type = token.tok_name[t.type]
 
-		# Add on what is found
 		tokens[-1].append(type)
 		text[-1].append(t.string)
 
-		# If the token is a newline then create a new nested list to add to
 		if type in ['NEWLINE', 'NL']:
 			tokens.append([])
 			text.append([])
 
-	# Keeps track of current indent in source code
 	indent = 0
 
-	# Stores html elements created in below loop
 	html = ''
 
-	# Generate html
 	for t_line, t_str in zip(tokens, text):
-		# Check if the next line should be indented or dedented
 		if t_line[0] == 'INDENT':
 			indent += 1
 		elif t_line[0] == 'DEDENT':
 			indent -= 1
 
-		# Each new line starts with a li tag and indent level even if 0
 		pass
 
-		# Parse each line formatting and indenting as needed
 		for tok, str in zip(t_line, t_str):
 			if tok == 'COMMENT':
 				pass
@@ -78,7 +65,6 @@ if __name__ == '__main__':
 					indent -= 1
 					pass
 
-			# Keep track of previous token
 			previous_tok = tok
 			previous_t_line = t_line
 			previous_str = str
@@ -86,7 +72,5 @@ if __name__ == '__main__':
 
 		html += li
 		print(li)
-		# print(indent, t_line, t_str)
 
-	# Add blank line to the end
 		pass
